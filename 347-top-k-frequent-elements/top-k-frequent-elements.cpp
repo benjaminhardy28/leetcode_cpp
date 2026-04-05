2defe944eb44ec4b9530ac2e4d5698ce034b6d00
Benjamin Hardy <107066058+benjaminhardy28@users.noreply.github.com>
2026-04-05 17:54:26 -0500

    Time: 119 ms (8.85%) | Memory: 19.2 MB (20.90%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> frequency_map;
+
+        for(auto num: nums){
+            frequency_map[num] += 1;
+        }
+
+        vector<int> bucket_count[nums.size()+1];
+
+        for (const auto& [key, value] : frequency_map) {
+            bucket_count[value].push_back(key);
+        }
+
+        vector<int> result;
+        int num_found = 0;
+        // loop from where i = nums.size() since the maximum possible frequency
+        // of an item is the size of the input array, which is an index of the size+1
+        for(int i=nums.size(); i>=0; i--){
+            cout << i;
+            if(num_found == k){
+                break;
+            }
+            cout << bucket_count[i].size();
+            if(bucket_count[i].size() > 0){
+                for(int val: bucket_count[i])
+                {
+                    cout << val;
+                    result.push_back(val);
+                    num_found++;
+                }
+            }
+        }
+        return result;
+    }
+};
