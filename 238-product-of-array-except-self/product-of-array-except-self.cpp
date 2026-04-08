98e798b8b2f79d4686fbb3e0b9f55973c03e0ffc
Benjamin Hardy <107066058+benjaminhardy28@users.noreply.github.com>
2026-04-07 20:11:19 -0500

    Time: 857 ms (5.00%) | Memory: 42.7 MB (6.96%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
+        int arrL[size];
+        int arrR[size];
+
+        int mult = 1;
+        arrL[0] = 1;
+        for(int i=1; i<size; i++){
+            arrL[i] = nums[i-1]*mult;
+            mult = nums[i-1]*mult;
+            cout << arrL[i] << "\n";
+        }
+
+        mult = 1;
+        arrR[size-1] = 1;
+        for(int i=size-2; i>=0; i--){
+            arrR[i] = nums[i+1]*mult;
+            mult = nums[i+1]*mult;
+            cout << arrR[i] << "\n";
+        }
+
+        vector<int> res;
+        for(int i=0; i<size; i++){
+            res.push_back(arrR[i]*arrL[i]);
+        }
+        return res;
+    }
+};
+
+//       [1,2,3,4]
+// left: [1,1,2,6]
+// right:[24,12,4,1]
