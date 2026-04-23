32625546548edff2f043fc030ee742622be714af
Benjamin Hardy <107066058+benjaminhardy28@users.noreply.github.com>
2026-04-22 23:04:30 -0500

    Time: 9 ms (53.70%) | Memory: 11.7 MB (66.25%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -13,18 +13,12 @@ public:
         for(int r=1; r<s.size(); r++){
             char cur = s[r];
 
-            cout << "pass l: " << l << s[l] << " r: " << r << s[r] << "\n";
-
-            if(last_index.contains(cur) && last_index[cur] > -1){
-                cout << "problem l: " << l << s[l] << " r: " << r << s[r] << "\n";
+            if(last_index.contains(cur)){
                 l = max(l, last_index[cur] + 1);
-                cout << "new l: " << l << s[l] << " r: " << r << s[r] << "\n";
             }
             last_index[cur] = r;
 
             maxLength = maxLength > (r - l + 1) ? maxLength : (r - l + 1);
-            cout << "max length" << maxLength << "\n";
-
         }
         return maxLength;
     }
